0ca3429956f9c46f2dae232b10a563671c75ff8e
hv236 <hel.verhaeghe@gmail.com>
2019-11-18 14:05:53 +0000

    no message

--- assignment/Sensors/HallSensor.m
+++ assignment/Sensors/HallSensor.m
@@ -2,25 +2,43 @@ classdef HallSensor < handle
     %HallSensor is a device that is used to measure the magnitude of a magnetic field.
     %Its output voltage is directly proportional to the magnetic field strength through it.
     %They are responsible of detecting the magnetic field of the magnets.
-  
+    
     
     properties
         %Pins
-        
+        VoutPin  %AnalogPin
+         
+        %Arduino
+        a
     end
     
     methods
-        function obj = untitled3(inputArg1,inputArg2)
-            %UNTITLED3 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        %Constructor
+        function this = HallSensor(a, AnalogPin)
+            
+            this.a=a;
+            this.VoutPin=AnalogPin;
+            
+            %Automatically configure the Analog Pin
+            configurePin(this.a, this.VoutPin, "AnalogInput");
+            
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function voltages = Read(this)
+            %readVoltage will simply read the voltages the sensor detects.
+          voltages = readVoltage(this.a,this.VoutPin);
+            
         end
+        
+        function []=pin(this)
+            %Utility method to print out the pins. Good tool if we forget.
+            
+            fprintf("Sensor Analog Pin:\n");
+            fprintf("   Vin\n");
+            fprintf(" %s \n",this.VoutPin);
+        end
+        
+        
     end
+    
 end
-
--- assignment/Sensors/HallSensorScript.m
+++ assignment/Sensors/HallSensorScript.m
@@ -4,7 +4,7 @@ clear
 george =arduino("COM5","Mega2560");
 
 
-Hello = HallSensor(george, "D8");
+Hello = HallSensor(george, "A8");
 
 voltages= zeros(500,1);
 
@@ -12,7 +12,7 @@ H=plot(voltages);
 
 while true
     
-V = Hello.readVoltage();
+V = Hello.Read();
 pause (0.01);
 
 voltages= [voltages(2:end);V];
