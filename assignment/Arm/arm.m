1d8c5ec2d97719b18271f6562eaab2276bca784b
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2019-11-29 13:10:12 +0000

    New version of arm to work with RC servo

--- assignment/Arm/arm.m
+++ assignment/Arm/arm.m
@@ -1,92 +1,115 @@
 classdef arm < handle
-    %ARM controls the height of the gripper.
-    %Programmed to be set in 3 different positions: low, high and drop cup.
-    %Powered by a stepper motor.
     
+    % ARM controls the height of the gripper.
+    % Programmed to be set in 3 different positions: low, high and drop cup.
+    % Powered by an RC servo motor.
+    
+    % Constants
+    properties (Constant)
+        
+        forwardDC = 0.2;
+        forwardSpeed = 300; % deg/s
+        reverseDC = 0.22;
+        reverseSpeed = 145; % deg/s
+        offDC = 0.21;
+        
+        minRotTime = 0.3; %s
+        
+        % THESE NEED CALIBRATING!
+        singleCupHeight = 30; % mm
+        cupVerticalSpacing = 10; % mm
+        
+    end
+    
+    % Public fields
     properties
-        %Arduino
-        a
+        a % Arduino
+    end
+    
+    % Private fields
+    properties (SetAccess = private)
         
-        pulsePin %PWM 
-        bluePin % DigitalOutput
-        yellowPin % DigitalOutput
-        pinkPin % DigitalOutput
-        orangePin % DigitalOutput
+        % Pins
+        pulsePin; % PWM
         
-        %Variable
-%         highPos= ;% setting up the nbr of steps required to get to this position
-%         lowPos=;
-%         dropPos=;
-%         cup5=; %height at which the top cup of stack is grabbed
-%         lowerCup=;%height to be lowered to grab the lowercup.
+        % State variables
+        currentAngle = 0; % 0 is UP
         
-        pins={d1};
     end
     
     methods
-        %Constructor
-        function this = arm(a, pulsePin, bluePin,yellowPin,pinkPin,orangePin)
+        
+        % Constructor
+        function this = arm(a, pulsePin)
             
-            this.a=a;
+            this.a = a;
             this.pulsePin = pulsePin;
-            this.bluePin = bluePin;
-            this.yellowPin = yellowPin;
-            this.pinkPin = pinkPin;
-            this.orangePin = orangePin;
             
-            %Automatically configure the Analog Pin
+            % Automatically configure the pulse pin
             configurePin(this.a, this.pulsePin, "PWM");
-            writePWMDutyCycle(this.a, this.pulsePin, 0.5);
-            configurePin(this.a, this.bluePin, "DigitalOutput");
-            configurePin(this.a, this.yellowPin, "DigitalOutput");
-            configurePin(this.a, this.pinkPin, "DigitalOutput");
-            configurePin(this.a, this.orangePin, "DigitalOutput");
+            writePWMDutyCycle(this.a, this.pulsePin, arm.offDC);
             
-%             function [] = printPins(this)
-%             % Utility method to print out the pins, in case we forget!
-%             fprintf("Arm control pins:\n");
-%             this.pins.print();
-%             
-            function [] = movetoheight(this,previousPos,newPos,UPorDown,pins)
-                %HEIGHT inputs: nbr of steps to reach desired position
-                % outputs:  rotation of motor
-                
-                %set REVERSE is going UP
-                up=reverse;
-                down=0;
-                
-              %%%function [] = movetoheight(this,height,reverse,pins)   
-              %add formula that converts that heigth into n-steps!!!
-
-                n= mod(previousPos,newPos); %determine the nbr of steps to be done to obtain final position, considering the previous position.
-            
-                switchMatrix = [
-                    1, 0, 1, 0;     %Step 1
-                    0, 1, 1, 0;     %Step 2
-                    0, 1, 0, 1;     %Step 3
-                    1, 0, 0, 1      %Step 4
-                    ];
-                
-                nRows = length(switchMatrix); 
-                
-                if reverse
-                    d = -1; %CCW
-                else
-                    d = 1;  %CW
-                end
-                for i = 1:n
-                    
-                    currentRow = switchMatrix(mod(d*i, nRows) + 1, :);
-                    prevRow = switchMatrix(mod(d*(i-1), nRows) + 1, :);
-                    
-                    for j = 1:4
-                        if currentRow(j) ~= prevRow(j)
-                            writeDigitalPin(this.a, pins{j}, currentRow(j));
-                        end           
-                    end
-                    
-
         end
+        
+        function [] = printPins(this)
+            % Utility method to print out the pins, in case we forget!
+            fprintf("Arm control pin (PWM): %s\n", this.pulsePin);
+        end
+        
+        function this = up(this)
+            % Moves the arm up to its highest position (clearance)
+            this.rotateTo(0);
+        end
+        
+        function this = down(this)
+            % Moves the arm down to its lowest position (sensing)
+            this.rotateTo(180);
+        end
+        
+        function this = pickupHeight(this, n)
+            % Moves the arm to the appropriate height to pick up a cup from
+            % the top of a stack of n cups
+            this.rotateTo();
+        end
+        
+        function this = stopMotor(this)
+            % Stops the motor
+            writePWMDutyCycle(this.a, this.pulsePin, arm.offDC);
+        end
+        
+        function this = rotateTo(this, angle)
+            
+            this.rotateMotor(angle - this.currentAngle);
+            
+        end
+        
+        function this = rotateMotor(this, angle)
+            
+            tic;
+            
+            if angle < 0
+                t = abs(angle)/arm.reverseSpeed;
+            else
+                t = abs(angle)/arm.forwardSpeed;
+            end
+            
+            if t < arm.minRotTime
+                error("Angle too small!");
+            end
+            
+            if angle < 0
+                writePWMDutyCycle(this.a, this.pulsePin, arm.reverseDC);
+            else
+                writePWMDutyCycle(this.a, this.pulsePin, arm.forwardDC);
+            end
+            
+            pause(t - toc);
+            
+            writePWMDutyCycle(this.a, this.pulsePin, arm.offDC);
+            
+            this.currentAngle = this.currentAngle + angle;
+            
+        end
+        
     end
 end
-
--- assignment/Arm/armPins.m
+++ assignment/Arm/armPins.m
@@ -3,19 +3,14 @@ classdef armPins < handle
     %   Detailed explanation goes here
     
     properties
-        pulsePin %PWM 
-        bluePin % DigitalOutput
-        yellowPin % DigitalOutput
-        pinkPin % DigitalOutput
-        orangePin % DigitalOutput
+        pulse
     end
     
     methods
-          function [] = print(this)
+        function [] = print(this)
             % Utility method to print out the pins, in case we forget!
-            fprintf("   blue    yellow      pulse    pink       orange  \n");
-            fprintf("X: %s        %s           %s       %s            %s \n", this.bluePin, this.yellowPin, this.pulsePin, this.pinkPin, this.orangePin);
-            fprintf("Y: %s        %s           %s       %s            %s \n", this.bluePin, this.yellowPin, this.pulsePin, this.pinkPin, this.orangePin);
+            fprintf("Pulse: %s\n", this.pulse);
+        end
     end
 end
 
