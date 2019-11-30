6c3f49562fb7da846716144ae1f33279c58a5067
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2019-11-30 17:50:34 +0000

    Last minute tweaks to treasure hunt script

--- TheTreasureHunt.m
+++ TheTreasureHunt.m
@@ -25,7 +25,8 @@ g = gantry(george, pins); % Initialise gantry
 sensor1 = HallSensor(george, "A0");
 sensor2 = HallSensor(george, "A1");
 sensor3 = HallSensor(george, "A2");
-m = m(george, "D3");
+
+m = arm(george, "D3");
 gripper = gripper(george, "D4");
 
 g.mode = gantryMode.PROGRAMMED; % Allow this script to control the updates
@@ -40,65 +41,65 @@ input('press to continue');
     
 %% SCAN FOR TREASURES! %%
 
-g.setSpeed(15); % Make it move slower for the scanning, it's more accurate
-m.down; % Scan at lowest arm position
-
-% ZigZagScan of the entire beach.
-% Outputs 3 matrices with signals detected from each sensor
-[v1, v2, v3] = zigzagScan(g, sensor1, sensor2, sensor3);
-
-% Combine the 3 signals into one long list
-v = [v1; v2; v3];
-
-% Assign columns to variables
-x = v(:, 1);
-y = v(:, 2);
-z = v(:, 3);
-
-% Offset data down by base voltage of hall sensor
-z = z - 2.55;
-
-% Take magnitude of values
-z = abs(z);
-
-%% PROCESS THE DATA! %%
-
-% Apply a threshold - only consider signals above that threshold
-% This cuts out all the noisy background data, we don't want to detect
-% hundreds of tiny peaks!
-for i = 1:size(z, 1)
-    for j= 1:size(z, 2)
-        if z(i,j) < 0.2
-            z(i,j) = 0;
-        end
-    end
-end
-
-% Clean the data - interpolate to get a more accurate data
-[xq, yq] = meshgrid(1:2:length(x), 1:2:length(y)); % 2mm steps between each x-y value
-
-G = griddata(x, y, z, xq, yq,'cubic'); % Interpolate the data recorded according to these finer x-y values
-
-G(isnan(G)) = 0;
-
-% Determine the epicenters from data
-
-treasurePeaks = imregionalmax(G); % Returns the binary image that identifies the regional maxima in matrix
-[Xpeaks, Ypeaks] = find(treasurePeaks == 1); % Returns the x-y coordinates of those peaks
-
-treasureCoord = [Xpeaks, Ypeaks]; % Creates 2-columns matrix with x-y coordinates
+% g.setSpeed(15); % Make it move slower for the scanning, it's more accurate
+% m.down; % Scan at lowest arm position
+% 
+% % ZigZagScan of the entire beach.
+% % Outputs 3 matrices with signals detected from each sensor
+% [v1, v2, v3] = zigzagScan(g, sensor1, sensor2, sensor3);
+% 
+% % Combine the 3 signals into one long list
+% v = [v1; v2; v3];
+% 
+% % Assign columns to variables
+% x = v(:, 1);
+% y = v(:, 2);
+% z = v(:, 3);
+% 
+% % Offset data down by base voltage of hall sensor
+% z = z - 2.55;
+% 
+% % Take magnitude of values
+% z = abs(z);
+% 
+% %% PROCESS THE DATA! %%
+% 
+% % Apply a threshold - only consider signals above that threshold
+% % This cuts out all the noisy background data, we don't want to detect
+% % hundreds of tiny peaks!
+% for i = 1:size(z, 1)
+%     for j= 1:size(z, 2)
+%         if z(i,j) < 0.05
+%             z(i,j) = 0;
+%         end
+%     end
+% end
+% 
+% % Clean the data - interpolate to get a more accurate data
+% [xq, yq] = meshgrid(1:2:length(x), 1:2:length(y)); % 2mm steps between each x-y value
+% 
+% G = griddata(x, y, z, xq, yq,'cubic'); % Interpolate the data recorded according to these finer x-y values
+% 
+% G(isnan(G)) = 0;
+% 
+% % Determine the epicenters from data
+% 
+% treasurePeaks = imregionalmax(G); % Returns the binary image that identifies the regional maxima in matrix
+% [Xpeaks, Ypeaks] = find(treasurePeaks == 1); % Returns the x-y coordinates of those peaks
+% 
+% treasureCoord = [Xpeaks, Ypeaks]; % Creates 2-columns matrix with x-y coordinates
 
 % Randomised treasure positions for testing gantry/arm/gripper movement
-% treasureCoord = [
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-%     rand * g.limits(1), rand * g.limits(2);
-% ];
+treasureCoord = [
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+    rand * g.limits(1), rand * g.limits(2);
+];
 
 % Classify from the furthest to closest 
 treasureOrder = sortrows(treasureCoord, 2, 'descend');
@@ -117,8 +118,8 @@ g.home; % Re-home just in case
 
 % 2 stacks of cups located: 1. under home 2. just next to 1 in y direction
 
-stack1XY = [0 0]; % Position of first stack
-stackSpacing = [0, 80]; % Offset to get to next stack
+stack1XY = [0, g.limits(2)]; % Position of first stack
+stackSpacing = [80, 0]; % Offset to get to next stack
 
 nCups = 5; % Number of cups per stack
 
@@ -141,7 +142,7 @@ for i = 1:numberTreasures % For each treasure location
     
     g.moveTo(stack1XY(1) + stackSpacing(1) * stackNumber, stack1XY(2) + stackSpacing(2) * stackNumber);
 
-    m.pickupHeight(this, cupNumber); % Lower the arm to top cup height
+    m.pickupHeight(cupNumber); % Lower the arm to top cup height
     gripper.grab;
     
     m.up; % Raise to high pos
